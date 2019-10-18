4a6bab7bb2ab878bdbe18d85d92306ae6be105c6
GuyCalloway <guyfcalloway@gmail.com>
2019-10-18 15:39:28 +0100

    Third test passing

--- spec/closest_string_spec.rb
+++ spec/closest_string_spec.rb
@@ -8,4 +8,8 @@ describe "Closest String" do
     it "returns array of two arrays with weight, index and number" do
         expect(closest("1 2")).to eq([[1, 0, 1], [2, 1, 2]])
     end
+
+    it "returns array of two arrays with weight, index and number" do
+        expect(closest("1 2 3")).to eq([[1, 0, 1], [2, 1, 2]])
+    end
 end
