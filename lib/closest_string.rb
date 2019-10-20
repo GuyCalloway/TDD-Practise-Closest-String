f78672067eea98f7cf685449657694697eeb911d
GuyCalloway <guyfcalloway@gmail.com>
2019-10-20 20:56:26 +0100

    Tests fell apart -- passes CodeWars KATA

--- lib/closest_string.rb
+++ lib/closest_string.rb
@@ -1,47 +1,45 @@
-def closest(arr)
-    if arr == ""
-        return []
+def closest(string)
+    original_number_array = string.split(" ").map{|x| x.to_i}
+    if string == ""
+      return []
     else
-        numbers = arr.split(" ")
-        weights = arr.split(" ").map{|x| z = x.split("")
-                                        sum(z) }
-        sorted_weights = weights.sort
-        pairs = find_pairs(sorted_weights)
-        p pairs
-        p sorted_weights
-        indices = find_index(pairs, weights)
-        find_orig(indices, weights, numbers) 
+      weights = weight_and_index_finder(string)
+      sorted_weights = weights.sort{|a, b| a[0] <=> b[0]}
+      pairs = find_pairs(sorted_weights)
+      find_result(pairs, original_number_array) 
     end
-end
-
-def sum(arr)
-    arr.inject(0){|sum,x| sum + x.to_i }
-end
-
-def find_pairs(sorted_arr)
-    diffs_and_pairs = []
-    sorted_arr.each_with_index{|x, i| diffs_and_pairs.push([(x - sorted_arr[i-1]).abs, [sorted_arr[i - 1], x]])}
-    diffs_and_pairs.sort!{ |a, b| a[0] <=> b[0]}
-    closest_pairs = diffs_and_pairs.select{|z| z[0] == diffs_and_pairs[0][0]}
-   return closest_pairs
-end
-
-def find_index(pairs, weights)
-    pairs.map! {|x| x[1]}
-    indices = []
-    pairs.each {|pair|  z = weights.map{|x| x}
-                       pair.each_with_index {|x, i| if i == 0
-                                         indices << [z.index(x)]
-                                         z.delete_at(z.index(x))
-                                          else
-                                            indices[-1] << z.index(x) + 1
-                                          end
-                    }
-                }
-                return indices
-end
-
-def find_orig(indices, weights, numbers)
-    x = indices.sort{ |a, b| sum(a) <=> sum(b) }
-    return [[weights[x[0][0]], x[0][0], numbers[x[0][0]].to_i], [weights[x[0][1]], x[0][1], numbers[x[0][1]].to_i]]
-end
+  end
+  
+    def weight_and_index_finder(string)
+      numbers = string.split(" ")
+      weights_and_index = split_sum_with_index(numbers)
+      weights_and_index
+    end
+    
+    def split_sum_with_index(numbers)
+      numbers.each_with_index.map  do |x, index| 
+        number = x.split("")
+        weight = sum(number)
+        [weight, index] 
+      end
+    end
+  
+    def sum(arr)
+      arr.inject(0){|sum,x| sum + x.to_i }
+    end
+  
+    def find_pairs(sorted_by_weight)
+      diffs_and_pairs = []
+      sorted_by_weight.each_with_index{|x, i| diffs_and_pairs.push([(x[0] - sorted_by_weight[i-1][0]).abs, [sorted_by_weight[i - 1][0], x[0]], [sorted_by_weight[i-1][1], x[1]]])}
+      closest_pairs = diffs_and_pairs.sort!{ |a, b| a[0]<=> b[0]}.select{|z| z[0] == diffs_and_pairs[0][0]}
+      return closest_pairs
+    end
+    
+    def find_result(array_with_pair_and_indices, original_number_array)
+      parent = array_with_pair_and_indices[0]
+      weight1 = parent[1][0]
+      weight2 = parent[1][1]
+      index1 = parent[2][0]
+      index2 = parent[2][1]
+      return [[weight1, index1, original_number_array[index1]], [weight2, index2, original_number_array[index2]]]
+    end
--- spec/closest_string_spec.rb
+++ spec/closest_string_spec.rb
@@ -1,15 +1,15 @@
 require 'closest_string'
 
-describe "Closest String" do
-    it "returns array of two arrays with weight, index and number" do
+describe "Closest" do
+    it "passes string with two identical numbers and formats output" do
         expect(closest("1 1")).to eq([[1, 0, 1], [1, 1, 1]])
     end
 
-    it "returns array of two arrays with weight, index and number" do
+    it "passes string with 2 sequential numbers and formats output" do
         expect(closest("1 2")).to eq([[1, 0, 1], [2, 1, 2]])
     end
 
-    it "returns array of two arrays with weight, index and number" do
+    it "passes string with 3 sequential numbers and formats output" do
         expect(closest("1 2 3")).to eq([[1, 0, 1], [2, 1, 2]])
     end
 
@@ -31,5 +31,19 @@ describe "Closest String" do
 
     it "returns array of two arrays with weight, index and number" do
         expect(closest("2 40 101 10 2 30 1")).to eq([[2, 0, 2], [2, 2, 101]])
+    end
+
+    it "returns array of two arrays with weight, index and number" do
+        expect(closest("2 40 101 10 2 30 101 2")).to eq([[2, 6, 101], [2, 7, 2]])
+    end
+
+    it "returns array of two arrays with weight, index and number" do
+        expect(closest("456899 50 11992 176 272293 163 389128 96 290193 85 52")).to eq([[13, 9, 85], [14, 3, 176]])
+        
+    end
+
+    it "returns array of two arrays with weight, index and number" do
+        
+       expect(closest("89998 187 126159 175 338292 89 39962 145 394230 167 1")).to eq([[13, 3, 175], [14, 9, 167]])
     end
 end
